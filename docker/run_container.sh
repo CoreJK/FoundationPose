f3ce6082480f493d8ca0fc01b910458e24adbc22
任伟明 <coreJK@163.com>
2024-09-27 15:08:56 -0700

    🖼 build: 修改了 容器的启动脚本，添加与相机相关权限的内容

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+
+# 移除已有的容器
 docker rm -f foundationpose
+
+# 设置工作目录
 DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /etc:/etc -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp --device=/dev/video4 --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
+
+# 允许 X11 连接
+if ! xhost +; then
+    echo "Failed to run xhost +"
+    exit 1
+fi
+
+# 运行 Docker 容器
+docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose \
+    --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged=true \
+    -v $DIR:$DIR -v /dev:/dev -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /tmp:/tmp -v /etc/udev:/etc/udev -v /run/udev:/run/udev --ipc=host \
+    -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
